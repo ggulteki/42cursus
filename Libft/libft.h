bdcc62e9edefb5bc96824a5ca92b0401e7fcc819
Gökberk Gültekin <118129672+ggulteki@users.noreply.github.com>
2023-02-19 13:33:43 +0300

    Update libft.h

--- Libft/libft.h
+++ Libft/libft.h
@@ -20,13 +20,13 @@ int		ft_isdigit(int x);
 int		ft_isalnum(int c);
 int		ft_isascii(int x);
 int		ft_isprint(int x);
-size_t	ft_strlen(const char *s);
+size_t		ft_strlen(const char *s);
 void		*ft_memset(void *b, int c, size_t len);
 void		ft_bzero(void *s, size_t n);
 void		*ft_memcpy(void *dst, const void *src, size_t n); //
 void		*ft_memmove(void *dst, const void *src, size_t len);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlcat(char *dst, const char *src, size_t dstsize);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 char		*ft_strchr(const char *s, int c);
@@ -54,13 +54,13 @@ typedef struct s_list
 	void		*content;
 	struct s_list	*next;
 }	t_list;
-t_list	*ft_lstnew(void *content);
+t_list		*ft_lstnew(void *content);
 void		ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
-t_list	*ft_lstlast(t_list *lst);
+t_list		*ft_lstlast(t_list *lst);
 void		ft_lstadd_back(t_list **lst, t_list *new);
 void		ft_lstdelone(t_list *lst, void (*del)(void *));
 void		ft_lstclear(t_list **lst, void (*del)(void *));
 void		ft_lstiter(t_list *lst, void (*f)(void *));
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
